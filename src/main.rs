e3e683d7a15f786e9901f84e1eb850fe36552789
James Jenkins <applesmacs@yahoo.com>
2018-02-26 00:15:17 -0500

    Move consts; tweak zoom, sim speed etc.; add sun

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,10 @@ use planet::Planet;
 
 const BACKGROUND: Color = [0.2, 0.2, 0.2, 1.0];
 
-// G * (m1*m2)/r^2
-const G: f64 = 6.67408e-11;           // m^3/(kg*s^2)
-const MASS_SUN : f64 = 1.989e30;      // kg
-const MASS_EART: f64 = 5.972e24;      // kg
-//const MASS_MOON: f64 = 7.34767309e22; // kg
-const MASS_MOON: f64 = 7.34767309e20; // kg
-
-const DIST_EART: f64 = 146e6;         // km
-const DIST_MOON: f64 = 384400.0;      // km
+//http://curious.astro.cornell.edu/about-us/41-our-solar-system/the-earth/orbit/
+//85-how-fast-does-the-earth-go-at-perihelion-and-aphelion-intermediate
+const DIST_EART: f64 = 152.10e6; // km
+const DIST_MOON: f64 = 384400.0; // km
 
 
 pub struct App {
@@ -40,7 +35,7 @@ impl App {
         self.gl.draw(args.viewport(), |ctr, gl| {
             clear(BACKGROUND, gl);
 
-            let zoom = 0.0018;
+            let zoom = 0.000004;
             let t = ctr.transform.trans((size.width as f64)/2.0,
                                         (size.height as f64)/2.0);
             let zt = t.scale(zoom, zoom);
@@ -55,13 +50,12 @@ impl App {
         let mut updates = Vec::new();
 
         for p in &self.universe {
-            updates.push(p.update(&self.universe, args.dt * 50.0));
+            updates.push(p.update(&self.universe, args.dt * 100000.0));
         }
 
         for i in 0..self.universe.len() {
             self.universe[i].pos = updates[i].0;
             self.universe[i].vel = updates[i].1;
-            //println!("new p: {:?}", self.universe[i].pos);
         }
     }
 }
@@ -82,36 +76,32 @@ fn main() {
 
     let sun = Planet {
         color: [0.8, 0.8, 0.0, 1.0],
-        mass:  MASS_SUN,
+        mass:  1.989e30,
         radius: 695_700.0,
         pos: [0.0, 0.0],
         vel: [0.0, 0.0],
         tiny: 0.0,
     };
 
-    let BASE = 0.0;
-
     let earth = Planet {
         color: [0.1, 0.8, 0.15, 1.0],
-        mass:  MASS_EART,
+        mass:  5.972e24,
         radius: 6_371.0,
-        pos: [BASE, 0.0],
-        //pos: [DIST_EART/12.0, 0.0],
-        //pos: [DIST_EART/6.0, 0.0],
-        vel: [0.0, 0.0],
-        tiny: 80.0,//100.0,
+        pos: [DIST_EART, 0.0],
+        vel: [0.0, -29.3],
+        tiny: 8.0,//100.0,
     };
 
     let moon = Planet {
         color: [0.7, 0.7, 0.7, 1.0],
-        mass:  MASS_MOON,
+        mass:  7.34767309e22,
         radius: 1_737.0,
-        pos: [BASE + DIST_MOON, 0.0],
-        vel: [-5.0, -10.0],
-        tiny: 20.0,
+        pos: [DIST_EART + DIST_MOON, 0.0],
+        vel: [0.0, earth.vel[1] + 1.0],
+        tiny: 3.0,
     };
 
-    //app.universe.push(sun);
+    app.universe.push(sun);
     app.universe.push(earth);
     app.universe.push(moon);
 
