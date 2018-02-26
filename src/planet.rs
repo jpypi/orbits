76ccd362628f4b0172c831e1507ce365e8c5eba5
James Jenkins <applesmacs@yahoo.com>
2018-02-26 00:13:36 -0500

    Clean up Planet

--- src/planet.rs
+++ src/planet.rs
@@ -4,8 +4,8 @@ use graphics::draw_state::DrawState;
 use graphics::math::*;
 use graphics::*;
 
-//const G: f64 = 6.67408e-11;           // m^3/(kg*s^2)
-const G: f64 = 6.67408e-20;           // km^3/(kg*s^2)
+//const G: f64 = 6.67408e-11;  //  m^3/(kg*s^2)
+const G: f64 = 6.67408e-20;    // km^3/(kg*s^2)
 
 pub struct Planet {
     pub color: Color,
@@ -24,7 +24,6 @@ impl Planet {
     pub fn update(&self, universe: &Vec<Planet>, dt: f64) -> (Vec2d, Vec2d) {
         let mut net_force = [0.0, 0.0];
 
-        //println!("My mass: {}", self.mass);
         for p in universe {
             if (p as *const _) != (self as *const _) {
                 let dist = sub(self.pos, p.pos);
@@ -34,18 +33,14 @@ impl Planet {
                 net_force = add(net_force, force);
             }
         }
-        //println!("net force: {:?}", net_force);
 
         let net_acc = mul_scalar(net_force, 1.0/self.mass);
-        //println!("net acc: {:?}", net_acc);
 
         // Update position
         let pos = add(self.pos, mul_scalar(self.vel, dt));
 
         // Update velocity
-        //let vel = self.vel;
         let vel = add(self.vel, mul_scalar(net_acc, dt));
-        //println!("vel: {:?}", vel);
 
         return (pos, vel);
     }
@@ -55,7 +50,6 @@ impl Planet {
                                zoomed_trans: Matrix2d,
                                gl: &mut G) {
         let e = Ellipse::new(self.color);
-        //let e = Ellipse::new_border(self.color, self.radius/1.1);
 
         e.draw([self.pos[0] - self.radius, self.pos[1] - self.radius,
                 self.radius * 2.0, self.radius * 2.0],
